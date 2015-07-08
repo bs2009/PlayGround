a59e32cc6118070b6dd0624de68b4b8b96f0a42b
bs2009 <bsong2009@gmail.com>
2015-07-08 16:20:57 -0500

    add pj3 folder

---
+++ PJ3/ClientConstants.swift
@@ -0,0 +1,27 @@
+//
+//  ClientConstants.swift
+//  P3
+//
+//  Created by William Song on 5/22/15.
+//  Copyright (c) 2015 Bill Song. All rights reserved.
+//
+
+import Foundation
+
+extension Client {
+    
+    // MARK: - Constants
+    struct Constants {
+        
+        static let ParseApplicationID       : String = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
+        static let ParseRestApiKey          : String = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+        static let udacityAPIURL            : String = "https://www.udacity.com/api/session"
+        static let UdacityStudentDataURL    : String = "https://www.udacity.com/api/users/"
+        static let ParseStudentLocationDataURL : String = "https://api.parse.com/1/classes/StudentLocation"
+        static let parseURL                 : String = "https://api.parse.com/1/classes/"
+        static let UdacitySignUpURL         : String = "https://www.udacity.com/account/auth#!/signup"
+        static let myUserDataKey            : String = "myUserDataKey"
+        static let maxNumStudents           : String = "?limit=100"
+        
+    }
+}
