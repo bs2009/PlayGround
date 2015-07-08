45280044094a1dc90105118bf4a8c38878a5dbd4
bs2009 <bsong2009@gmail.com>
2015-07-08 14:11:59 -0500

    Add folder

---
+++ BondVillains/BondVillains/VillainDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  VillainDetailViewController.swift
+//  BondVillains
+//
+//  Created by William Song on 4/19/15.
+//  Copyright (c) 2015 Udacity. All rights reserved.
+//
+
+ import UIKit
+class VillainDetailViewController: UIViewController {
+    
+}
+
+
+
+
+//Checkout the branch, step5.5-bondVillains, and consult the following steps as you create the Bond Villains Master Detail app:
+//
+//Setting up the navigation structure
+//
+//Add a Navigation Controller
+//Make the Villain Table the root view
+//Creating and preparing the DetailViewController
+//
+//Create a VillainDetailViewController with a 100x80 image view, and a label
+//Give the VillainDetailViewController a Storyboard ID
+//Give the VillainDetailViewController a villain property
+//Write the viewWillAppear method in the VillainDetailViewController so that it sets the image and label
+//Make ViewController the delegate of its table view
+//Presenting the DetailViewController
+//
+//Add the tableView(tableView: UITableView, didSelectRowAtIndexPath) method to ViewController
+//When a row is selected, push the detail view controller onto the navigation stack.
+//For the completed code for Bond Villains with Master-Detail checkout step5.6-bondVillains-masterDetail
